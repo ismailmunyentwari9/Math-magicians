3ddbff98a9f3e08e2670d504f7036a1fa6cfe809
Ismail <ismailmunyentwari9@gmail.com>
2023-03-22 19:04:45 +0200

    Add logics to math_magician

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,17 +1,24 @@
 import './calc.css';
+import React, { useState } from 'react';
+import Calculate from './logic/calculate';
 import Buttons from './buttons';
+import ScreenValue from './logic/screenValue';
 
-function calculator() {
+function Calculator() {
+  const [result, setResult] = useState(0);
+  const handleClick = (e) => {
+    setResult((obj) => Calculate(obj, e.target.innerText));
+  };
+
   return (
     <div className="calc-container">
 
       <div className="output">
-
-        <div className="current-operand" />
+        <ScreenValue result={result.next || result.total || '0'} />
       </div>
-      <Buttons />
+      <Buttons handleClick={handleClick} />
 
     </div>
   );
 }
-export default calculator;
+export default Calculator;
